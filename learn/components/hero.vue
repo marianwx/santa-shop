14cd891ce74fd8f3edde92be31e52c1b52085854
marianwx <maransmk69@gmail.com>
2024-01-17 17:47:14 +0200

    .

--- learn/components/hero.vue
+++ learn/components/hero.vue
@@ -1,7 +1,7 @@
 <template>
 
 <div class="bg-white pb-6 sm:pb-8 lg:pb-12">
-  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+  <div class="container mx-auto max-w-screen-2xl px-4 md:px-8">
 
     <section class="flex flex-col justify-evenly gap-6 sm:gap-10 md:gap-16 lg:flex-row">
       <!-- content - start -->
@@ -13,8 +13,8 @@
         <p class="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random.</p>
 
         <div class="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
-          <a href="#" class="inline-block rounded-lg bg-green-600 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Explorează</a>
-
+          <a href="#" class="inline-block rounded-lg bg-green-600 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:opacity-90 focus-visible:ring active:bg-indigo-700 md:text-base">Explorează</a>
+ 
           <a href="#" class="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Contact</a>
         </div>
       </div>
@@ -22,7 +22,7 @@
 
       <!-- image - start -->
       <div class="h-48 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:h-auto xl:w-5/12">
-        <img src="http://tinyurl.com/pts5n25v" loading="lazy" alt="Photo by Fakurian Design" class="h-full w-full object-cover object-center" />
+        <img src="http://tinyurl.com/yc3c2rv5" loading="lazy" alt="Photo by Fakurian Design" class="h-full w-full object-cover object-center" />
       </div>
       <!-- image - end -->
     </section>
