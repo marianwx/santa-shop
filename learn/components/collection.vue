7e26366cfd49fccb2dbb5f2340374e043cd18908
maran <maransmk69@gmail.com>
2024-01-21 14:25:20 +0200

    .

--- learn/components/collection.vue
+++ learn/components/collection.vue
@@ -1,11 +1,11 @@
 <template>
 
-<div class="py-40 flex flex-col justify-center">
+<div class="py-36 flex flex-col justify-center">
   <div class="relative m-3 flex flex-wrap mx-auto justify-center">
 
     <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
       <div class="overflow-x-hidden rounded-2xl relative">
-        <img class="h-40 rounded-2xl w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg">
+        <img class="h-60 rounded-2xl w-full object-cover" src="https://media.discordapp.net/attachments/1183688301157306400/1185868440125325372/coronita-rosie.jpg?ex=65b6170c&is=65a3a20c&hm=0db648df4995ef0517f57f632abb6adda15992e2f75c4ada634f16e8b7262331&=&format=webp&width=502&height=670">
         <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -14,8 +14,8 @@
       </div>
       <div class="mt-4 pl-2 mb-2 flex justify-between ">
         <div>
-          <p class="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-          <p class="text-md text-gray-800 mt-0">$340</p>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Coroniță</p>
+          <p class="text-md text-gray-800 mt-0">44.99 RON</p>
         </div>
         <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
@@ -27,7 +27,7 @@
 
     <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
       <div class="overflow-x-hidden rounded-2xl relative">
-        <img class="h-40 rounded-2xl w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg">
+        <img class="h-60 rounded-2xl w-full object-cover" src="https://media.discordapp.net/attachments/1183688301157306400/1187507268753821726/glob-inima.png?ex=65bc0d53&is=65a99853&hm=e196a4f30fde139301020b5e919c4080e269fddd5842a4cb91f56a9e38024fd9&=&format=webp&quality=lossless&width=502&height=670">
         <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -36,8 +36,8 @@
       </div>
       <div class="mt-4 pl-2 mb-2 flex justify-between ">
         <div>
-          <p class="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-          <p class="text-md text-gray-800 mt-0">$340</p>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Globuleț Inimioară</p>
+          <p class="text-md text-gray-800 mt-0">19.99</p>
         </div>
         <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
@@ -49,7 +49,7 @@
 
     <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
       <div class="overflow-x-hidden rounded-2xl relative">
-        <img class="h-40 rounded-2xl w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg">
+        <img class="h-60 rounded-2xl w-full object-cover" src="https://media.discordapp.net/attachments/1183688301157306400/1187514933705973790/WhatsApp_Image_2023-12-03_at_21.56.21_e22fed82.jpg?ex=65bc1477&is=65a99f77&hm=a272176ef4362fe2fd0dc5f206d596e9ccb21f0e838b77466368b780e861102e&=&format=webp&width=502&height=670">
         <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -58,8 +58,8 @@
       </div>
       <div class="mt-4 pl-2 mb-2 flex justify-between ">
         <div>
-          <p class="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-          <p class="text-md text-gray-800 mt-0">$340</p>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Brioșe</p>
+          <p class="text-md text-gray-800 mt-0">5.99 RON</p>
         </div>
         <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
@@ -71,7 +71,7 @@
 
     <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-2 mx-1 my-3 cursor-pointer">
       <div class="overflow-x-hidden rounded-2xl relative">
-        <img class="h-40 rounded-2xl w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg">
+        <img class="h-60 rounded-2xl w-full object-cover" src="https://media.discordapp.net/attachments/1183688301157306400/1187503749984424016/bradut-lemn.jpg?ex=65bc0a0c&is=65a9950c&hm=a92714f9afbad406bb99abbf48714c1dc4f7aecdb80765372cd205ab705be0d7&=&format=webp&width=502&height=670">
         <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -80,8 +80,8 @@
       </div>
       <div class="mt-4 pl-2 mb-2 flex justify-between ">
         <div>
-          <p class="text-lg font-semibold text-gray-900 mb-0">Product Name</p>
-          <p class="text-md text-gray-800 mt-0">$340</p>
+          <p class="text-lg font-semibold text-gray-900 mb-0">Brăduț Luminiță</p>
+          <p class="text-md text-gray-800 mt-0">24.99 RON</p>
         </div>
         <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
